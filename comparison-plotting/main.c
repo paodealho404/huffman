b644e669a1ffa2ecfd6b8486494ba69bfa4d1ddc
Joao Falcao <joao.falcao@icloud.com>
2020-02-02 01:31:28 -0300

    plot de mais figuras; requirements.txt

--- comparison-plotting/main.c
+++ comparison-plotting/main.c
@@ -1,58 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "binary_tree.h"
 #include "avl.h"
+#define N 25000
 
 int main()
 {
-    int i = 0, n;
-    int comp_bt = 0, comp_avl = 0;
+    int i = 0, n, min = -1001, max = 1;
+    int comp_bt_r = 0, comp_bt_l = 0, comp_avl_r = 0, comp_avl_l = 0;
+    
+    srand((unsigned) time(NULL));
 
-    BINARY_TREE *bt = create_empty_binary_tree();
-    AVL *avl = create_empty_avl();
+    BINARY_TREE *bt_r = create_empty_binary_tree();
+    BINARY_TREE *bt_l = create_empty_binary_tree();
+    AVL *avl_r = create_empty_avl();
+    AVL *avl_l = create_empty_avl();
 
-    FILE *file = fopen("number_list.txt", "r");
+    FILE *input = fopen("number_list.txt", "r");
 
-    printf("Aguarde a leitura dos 50000 numeros :)\n");
-    fscanf(file, "%d", &n);
-    while(!feof(file))
+    printf("Aguarde a leitura dos numeros :)\n");
+    fscanf(input, "%d", &n);
+    while(!feof(input))
     {
         i++;
 
-        bt = add_bt(bt, n);
+        bt_r = add_bt(bt_r, n);
+        bt_l = add_bt(bt_l, i);
 
-        avl = add_avl(&(avl), avl, n);
+        avl_r = add_avl(&(avl_r), avl_r, n);
+        avl_l = add_avl(&(avl_l), avl_l, i);
 
-        fscanf(file, "%d", &n);
-        printf("%d de 50000... \r", i);
+        fscanf(input, "%d", &n);
+        printf("%d de %d... \r", i, N);
     }
-    fclose(file);
+    fclose(input);
+    
+    printf("\nLeitura finalizada, iniciando sorteios\n");
 
-    printf("\n");
-
-    printf("\nentre o numero a ser procurado (1 - 50000): \n> ");
-    while(1)
+    FILE *output = fopen("out.txt", "a");
+    for(i = 0; i < 100; i++)
     {
-        scanf("%d", &n);
-        
-        BINARY_TREE *n1 = search_bt(bt, n, &comp_bt);
-        AVL *n2 = search_avl(avl, n, &comp_avl);
+        if(i % 4 == 0)
+        {
+            min += 1000;
+            max += 1000;
+        }
 
-        if(!is_bt_empty(n1) && !is_avl_empty(n2))
-            printf("%d encontrado!\n", n);
-        else
-            printf("%d nao encontrado :(\n", n);
-        
-        printf("ABB: %d comparacoes\n", comp_bt);
-        printf("AVL: %d comparacoes\n", comp_avl);
+        n = rand() % (max + 1 - min) + min;
+        printf("Numero sorteado: %d\n", n);
 
-        FILE *file = fopen("out.txt", "a");
-        fprintf(file, "%d %d %d\n", n, comp_bt, comp_avl);
-        fclose(file);
+        BINARY_TREE *n1 = search_bt(bt_r, n, &comp_bt_r);
+        BINARY_TREE *n2 = search_bt(bt_l, n, &comp_bt_l);
 
-        comp_bt = 0;
-        comp_avl = 0;
+        AVL *n3 = search_avl(avl_r, n, &comp_avl_r);
+        AVL *n4 = search_avl(avl_l, n, &comp_avl_l);
+
+        fprintf(output, "%d %d %d %d %d\n", n, comp_bt_r, comp_bt_l, comp_avl_r, comp_avl_l);
+
+        comp_bt_r = 0;
+        comp_bt_l = 0;
+        comp_avl_r = 0;
+        comp_avl_l = 0;
     }
+    fclose(output);
 
     return 0;
 }
--- comparison-plotting/plot.py
+++ comparison-plotting/plot.py
@@ -1,41 +1,51 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
 import os, sys
 import numpy as np
 import matplotlib as mpl
 import matplotlib.cbook as cb
 import matplotlib.pyplot as plt
 
-
-# %%
 def read_file(fname):
-    data = np.genfromtxt(fname, dtype=int, names=['n', 'abb', 'avl'])
+    data = np.genfromtxt(fname, dtype=int, names=['n', 'abb_r', 'abb_l', 'avl_r', 'avl_l'])
     return data
 
-
-# %%
-notebook_path = os.path.abspath("plot.ipynb")
+notebook_path = os.path.abspath("plot.py")
 file_path = os.path.join(os.path.dirname(notebook_path), "out.txt")
 
 data = read_file(file_path)
 data.sort(axis=0)
 
-fig = plt.figure(figsize=[10,7])
-
-ax = plt.subplot()
-
-ax.plot(data['n'], data['abb'], 'o-', label="ABB", lineWidth=2)
-ax.plot(data['n'], data['avl'], 'o-', label="AVL", lineWidth=2)
+fig1 = plt.figure(figsize=[10,7])
+ax = fig1.add_subplot()
+ax.plot(data['n'], data['abb_r'], 'o-', label="ABB (Random)", lineWidth=1)
+ax.plot(data['n'], data['avl_r'], 'o-', label="AVL (Random)", lineWidth=1)
 ax.legend()
-
-ax.set(xlabel='Número Procurado', ylabel='Quantidade de Comparações', title="ABB vs. AVL")
-
+ax.set(xlabel='Número Procurado', ylabel='Quantidade de Comparações', title="ABB vs. AVL: Entrada Aleatória")
 ax.yaxis.set_major_locator(mpl.ticker.MultipleLocator(2))
 ax.yaxis.set_minor_locator(mpl.ticker.MultipleLocator(1))
-
 ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(5000))
-ax.xaxis.set_minor_locator(mpl.ticker.MultipleLocator(2500))
+ax.xaxis.set_minor_locator(mpl.ticker.MultipleLocator(1000))
+
+fig2 = plt.figure(figsize=[10,7])
+ax2 = fig2.add_subplot()
+ax2.plot(data['n'], data['abb_r'], 'o-', label="ABB (Random)", lineWidth=1)
+ax2.plot(data['n'], data['avl_l'], 'o-', label="AVL (Linear)", lineWidth=1)
+ax2.plot(data['n'], data['avl_r'], 'o-', label="AVL (Random)", lineWidth=1)
+ax2.legend()
+ax2.set(xlabel='Número Procurado', ylabel='Quantidade de Comparações', title="ABB vs. AVL")
+ax2.yaxis.set_major_locator(mpl.ticker.MultipleLocator(2))
+ax2.yaxis.set_minor_locator(mpl.ticker.MultipleLocator(1))
+ax2.xaxis.set_major_locator(mpl.ticker.MultipleLocator(5000))
+ax2.xaxis.set_minor_locator(mpl.ticker.MultipleLocator(1000))
+
+fig3 = plt.figure(figsize=[10,7])
+ax3 = fig3.add_subplot()
+ax3.plot(data['n'], data['abb_l'], 'o-', label="ABB (Linear)", lineWidth=1)
+ax3.plot(data['n'], data['avl_l'], 'o-', label="AVL (Linear)", lineWidth=1)
+ax3.legend()
+ax3.set(xlabel='Número Procurado', ylabel='Quantidade de Comparações', title="ABB vs AVL: Entrada Linear")
+ax3.yaxis.set_major_locator(mpl.ticker.MultipleLocator(5000))
+ax3.yaxis.set_minor_locator(mpl.ticker.MultipleLocator(1000))
+ax3.xaxis.set_major_locator(mpl.ticker.MultipleLocator(5000))
+ax3.xaxis.set_minor_locator(mpl.ticker.MultipleLocator(1000))
 
 plt.show()
-
